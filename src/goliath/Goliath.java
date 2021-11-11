59b04d0cc82b809da2aae33b1b5169bd76a904ee
Enrique Ferrer Cebrian <enriqueferrercebrian@gmail.com>
2021-11-11 15:47:23 +0100

    ya esta

--- src/goliath/Goliath.java
+++ src/goliath/Goliath.java
@@ -7,9 +7,7 @@ package goliath;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.Scanner;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,28 +20,45 @@ public class Goliath {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String iban, nomTitular, remitente, concepto, tipoMovimiento;
-        int cantidad;
-
+        String iban = "", nomTitular, remitente = "", concepto = "", stringConcepto = "Concepto: ", stringRemitente = "Remitente: ", tipoMovimiento, cantidadRetirada = "Cantidad retirada: ", cantidadIngresada = "Cantidad ingresada: ";
+        int cantidad = 0;
+        boolean ibancorrecto = false;
         Date fecha;
 
         Scanner sc = new Scanner(System.in);
         int respuestaUsuario = 0;
-        System.out.println("Bienvenido, Indiqueme su IBAN");
-        iban = sc.nextLine();
-        System.out.println("¿Podria, usted, proporcionarme su nombre?");
+        while (ibancorrecto == false) {
+            System.out.println("Indiqueme su IBAN (ES+22 dígitos) ");
+
+            iban = sc.nextLine();
+            iban = iban.toUpperCase();
+            if (iban.length() == 24 && iban.substring(0, 2).equalsIgnoreCase("ES")) {
+                ibancorrecto = true;
+            } else {
+                System.out.println("Formato de IBAN no sorportado.");
+            }
+        }
+
+        System.out.println(
+                "Introduce el nombre del titular");
         nomTitular = sc.nextLine();
+
+        System.out.println(
+                "Bienvenido/a a GNB");
+        System.out.println(
+                "It's gonna be legen...dary!");
         boolean seguir = true;
 
         //revisar iban que este correcto
         CompteBancari nuevaCuenta = new CompteBancari(iban, nomTitular);
 
         //Movimiento nuevoIngreso = new Movimiento("ingreso" ,100, "asdf", "sdf", Calendar.getInstance().getTime());
-       // nuevaCuenta.ingreso(nuevoIngreso);
+        // nuevaCuenta.ingreso(nuevoIngreso);
+        while (seguir
+                == true) {
+            System.out.println("");
+            System.out.println("Selecciona una opción.");
 
-        while (seguir == true) {
-
-            System.out.println("Que quieres hacer?");
             System.out.println("1. Muestrane mis datos bancarios.");
             System.out.println("2. Muestrame mi IBAN.");
             System.out.println("3. Muestrame el titular de la cuenta.");
@@ -79,49 +94,60 @@ public class Goliath {
                     break;
 
                 case 5:
-                    System.out.println("Cuanto?");
+                    System.out.println("Cantidad ingresada (sin decimales): ");
                     cantidad = Integer.parseInt(sc.nextLine());
 
-                    System.out.println("quien?");
+                    System.out.println("Remitente: ");
                     remitente = sc.nextLine();
 
-                    System.out.println("Concepto?");
+                    System.out.println("Concepto: ");
                     concepto = sc.nextLine();
 
                     fecha = Calendar.getInstance().getTime();
                     tipoMovimiento = "Ingreso ";
 
-                    Movimiento nuevoIngreso2 = new Movimiento(tipoMovimiento ,cantidad, remitente, concepto, fecha);
+                    Movimiento nuevoIngreso2 = new Movimiento(tipoMovimiento, cantidad, remitente, concepto, fecha);
 
                     nuevaCuenta.ingreso(nuevoIngreso2);
+                    System.out.println("*Ingreso realizado con éxito*");
+                    System.out.println("");
+
+                    System.out.println("Saldo: " + nuevaCuenta.getSaldo());
+                    System.out.println("");
 
                     break;
 
                 case 6:
 
-                    System.out.println("Cuanto?");
+                    System.out.println("Cantidad retirada (sin decimales): ");
                     cantidad = Integer.parseInt(sc.nextLine());
 
-                    System.out.println("quien?");
+                    System.out.println("Remitente: ");
                     remitente = sc.nextLine();
 
-                    System.out.println("Concepto?");
+                    System.out.println("Concepto: ");
                     concepto = sc.nextLine();
-                    
+
                     fecha = Calendar.getInstance().getTime();
-                     tipoMovimiento = "Retirada ";
+                    tipoMovimiento = "Retirada";
 
                     Movimiento nuevaRetirada = new Movimiento(tipoMovimiento, cantidad, remitente, concepto, fecha);
 
                     nuevaCuenta.retirada(nuevaRetirada);
+                    System.out.println("*Retirada realizada con éxito*");
+                    System.out.println("");
+
+                    System.out.println("Saldo: " + nuevaCuenta.getSaldo());
+                    System.out.println("");
 
                     break;
 
                 case 7:
-
+                    System.out.println("                             Historico de movimientos");
                     List<Movimiento> lista = nuevaCuenta.getHistoricoMovimientos();
                     for (int i = 0; i < lista.size(); i++) {
-                        System.out.println(lista.get(i).getTipoMovimiento() + " " +  lista.get(i).getCantidad() + " " + lista.get(i).getRemitente() + " " + lista.get(i).getConcepto() + " " + lista.get(i).getFecha() + " "
+                        System.out.println("---------------------------------------------------------------------------");
+                        System.out.println(lista.get(i).getTipoMovimiento() + " | " + cantidadRetirada + lista.get(i).getCantidad() + " | " + stringRemitente + lista.get(i).getRemitente() + " | " + stringConcepto + lista.get(i).getConcepto() + " ---> " + lista.get(i).getFecha() + " "
                         );
                     }
                     break;
